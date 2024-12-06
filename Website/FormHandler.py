d817e725d4a7ff32cb8ea6cf5301a409bab0da99
DawidP2001 <c00273530@setu.ie>
2024-12-06 13:00:55 +0000

    Preparing for deployment

--- Website/FormHandler.py
+++ Website/FormHandler.py
@@ -4,7 +4,7 @@
 
 from flask import Flask, request, render_template, session
 from Utility import Reddit as r
-#from Utility import SenitmentAnalyser as s
+from Utility import SenitmentAnalyser as s
 from Utility import Utils
 import os
 from collections import Counter
@@ -39,11 +39,11 @@ def submit():
     keyList, itemList = Utils.convertSubOccurencesForJs(Counter(subbredditList))
     
 
-    #session['sentimentList'] = s.analyseSentiment(titleList) # Contains the result of sentiment analysis
+    sentimentList = s.analyseSentiment(titleList) # Contains the result of sentiment analysis
 
     ######### JUST FOR TESTING
-    sentimentList = [{'Text': 'Multiple far right/facist protestors seen in Poland today.', 'label': 'NEGATIVE', 'score': 0.7135903835296631}, {'Text': 'American guy in Poland calls Indian racial slurs and most invasive species', 'label': 'NEGATIVE', 'score': 0.9918959736824036}, {'Text': 'Poland when Russia violates their airspace.', 'label': 'NEGATIVE', 'score': 0.9741829037666321}, {'Text': '“You forgot Poland”: criticism as US, UK, France and Germany meet alone to discuss Ukraine', 'label': 'NEGATIVE', 'score': 0.9898450374603271}, {'Text': 'Crowley the gnome deported from Poland and banned from entering EU for 10 years', 'label': 'NEGATIVE', 'score': 0.9892055988311768}, {'Text': 'How DID Poland become safe?', 'label': 'NEGATIVE', 'score': 0.9966275095939636}, {'Text': 'Poland tells Ukraine to exhume second world war victims even amid Russia’s invasion', 'label': 'POSITIVE', 'score': 0.969015896320343}, {'Text': 'Moving from Australia to Poland, are we crazy?', 'label': 'NEGATIVE', 'score': 0.9575971961021423}, {'Text': 'How would you describe your experiences in Poland?', 'label': 'NEGATIVE', 'score': 
-0.9826070666313171}, {'Text': 'Poland seeks British help to protect Ukraine after Trump win', 'label': 'NEGATIVE', 'score': 0.9199395775794983}]
+   # sentimentList = [{'Text': 'Multiple far right/facist protestors seen in Poland today.', 'label': 'NEGATIVE', 'score': 0.7135903835296631}, {'Text': 'American guy in Poland calls Indian racial slurs and most invasive species', 'label': 'NEGATIVE', 'score': 0.9918959736824036}, {'Text': 'Poland when Russia violates their airspace.', 'label': 'NEGATIVE', 'score': 0.9741829037666321}, {'Text': '“You forgot Poland”: criticism as US, UK, France and Germany meet alone to discuss Ukraine', 'label': 'NEGATIVE', 'score': 0.9898450374603271}, {'Text': 'Crowley the gnome deported from Poland and banned from entering EU for 10 years', 'label': 'NEGATIVE', 'score': 0.9892055988311768}, {'Text': 'How DID Poland become safe?', 'label': 'NEGATIVE', 'score': 0.9966275095939636}, {'Text': 'Poland tells Ukraine to exhume second world war victims even amid Russia’s invasion', 'label': 'POSITIVE', 'score': 0.969015896320343}, {'Text': 'Moving from Australia to Poland, are we crazy?', 'label': 'NEGATIVE', 'score': 0.9575971961021423}, {'Text': 'How would you describe your experiences in Poland?', 'label': 'NEGATIVE', 'score': 
+#0.9826070666313171}, {'Text': 'Poland seeks British help to protect Ukraine after Trump win', 'label': 'NEGATIVE', 'score': 0.9199395775794983}]
     #########
 
     positiveSentimentList, neutralSentimentList, negativeSentimentList  = Utils.seperateSentiments(sentimentList)
