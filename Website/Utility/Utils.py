9e3af1a3e61e7909db3125ff3fdcb4f14ab3a8f8
DawidP2001 <c00273530@setu.ie>
2025-01-30 10:30:03 +0000

    Improved the styling for charts as well as sorted the subreddit bar chart

--- Website/Utility/Utils.py
+++ Website/Utility/Utils.py
@@ -9,7 +9,7 @@ from collections import Counter
 
 import praw
 #from Utility import SenitmentAnalyser as s
-from Utility import SenitmentAnalyser as s
+from Utility import testSenitmentAnalyser as s
 import datetime
 
 def countLables(positiveSentimentList, neutralSentimentList, negativeSentimentList):
@@ -26,23 +26,20 @@ def convertSubOccurencesForJs(subredditCounter):
     for key in keys:
         keyList.append(key)
         itemList.append(subredditCounter[key])
-
+    keyList, itemList = sortSubredditOccurences(keyList, itemList)
     return keyList, itemList
-
-########### JUST FOR TESTING
-def seperateSentimentsTest(sentimentList):
-    positiveSentimentList = []
-    neutralSentimentList = []
-    negativeSentimentList = []
-
-    for value in sentimentList:
-        if(value['label'] == 'POSITIVE'):
-            positiveSentimentList.append(value)
-        if(value['label'] == 'NEUTRAL'):
-            neutralSentimentList.append(value)
-        if(value['label'] == 'NEGATIVE'):
-            negativeSentimentList.append(value)
-    return positiveSentimentList, neutralSentimentList, negativeSentimentList
+# Sorts the items in the lists in descending order
+def sortSubredditOccurences(keyList, itemList):
+    for i in range(len(itemList)):
+        for j in range(i, len(itemList)):
+            if itemList[i] < itemList[j]:
+                temp = itemList[i]
+                itemList[i] = itemList[j]
+                itemList[j] = temp
+                temp = keyList[i]
+                keyList[i] = keyList[j]
+                keyList[j] = temp
+    return keyList, itemList
 
 # This function takes a list of dictionaries and seperates them into 3 lists based on their sentiment
 # Also it adds the sentiment and score to the dictionary
